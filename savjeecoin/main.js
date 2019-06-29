e2b54427d768e6c417e0a33d2ac07d26c2467e3f
Anthony Leung <atony.leung.canada@mail.utoronto.ca>
2019-06-29 16:53:18 -0400

    Second commit. Video 2 done

--- savjeecoin/main.js
+++ savjeecoin/main.js
@@ -7,6 +7,7 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash() {
@@ -14,14 +15,27 @@ class Block {
       this.index + 
       this.previousHash + 
       this.timestamp + 
+      this.nonce + 
       JSON.stringify(this.data)
     ).toString();
   }
+
+  mineBlock(difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log("Block mined: " + this.hash);
+  }
 }
+
+
 
 class Blockchain {
   constructor () {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
   }
 
   createGenesisBlock() {
@@ -34,7 +48,7 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     // In reality, there'd be a lot more checks before this is added
     this.chain.push(newBlock);
   }
@@ -55,6 +69,8 @@ class Blockchain {
 }
 
 let savjeeCoin = new Blockchain();
+
+/* From video one
 savjeeCoin.addBlock(new Block(1, "10/01/2019", { amount: 4 }));
 savjeeCoin.addBlock(new Block(2, "12/07/2019", { amount: 10 }));
 
@@ -65,4 +81,11 @@ savjeeCoin.chain[1].hash = savjeeCoin.chain[1].calculateHash();
 
 console.log('Is blockchain valid? ' + savjeeCoin.isChainValid());
 
-console.log(JSON.stringify(savjeeCoin, null, 2));
+console.log(JSON.stringify(savjeeCoin, null, 2)); 
+*/
+
+console.log('Mining block 1...');
+savjeeCoin.addBlock(new Block(1, "10/01/2019", { amount: 4 }));
+
+console.log('Mining block 2...');
+savjeeCoin.addBlock(new Block(2, "12/07/2019", { amount: 10 }));
